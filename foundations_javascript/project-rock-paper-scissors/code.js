b20cd3c1a6873a7addf06f51ec8859d0e001cfe5
Jorge Serna <133090449+alacrancoder@users.noreply.github.com>
2025-05-03 01:40:15 -0500

    Completed steps 1 through 5

--- foundations_javascript/project-rock-paper-scissors/code.js
+++ foundations_javascript/project-rock-paper-scissors/code.js
@@ -1,24 +1,57 @@
+
 function getComputerChoice() {
     let decider = Math.floor(Math.random() * 100)
     let choice = null
     console.log(decider)
     if (decider < 33){
-        choice = "Rock"
+        choice = "rock"
     } else if (decider >= 33 && decider < 66) {
-        choice = "Paper"
+        choice = "paper"
     } else {
-        choice = "Scissors"
+        choice = "scissors"
     }
     return choice
 }
 
-computer = getComputerChoice()
 
-console.log("Computer: ", computer)
+
+//console.log("Computer: ", computer)
 
 function getHumanChoice() {
     let humanChoice = prompt("Enter your choice!")
+    humanChoice = humanChoice.toLowerCase()
     return humanChoice
 }
-human = getHumanChoice()
-console.log("Human: ", human)
+//console.log("Human: ", human)
+
+let humanScore = 0
+let computerScore = 0
+
+function playRound(getHumanChoice, getcomputerChoice){
+   if (humanChoice === computerChoice){
+    console.log("It's a tie!")
+    return null
+   } else if (humanChoice === "rock" && computerChoice === "scissors"){
+    console.log("You win!")
+    humanScore = humanScore++
+    return humanScore
+   } else if (humanChoice === "paper" && computerChoice === "rock"){
+    console.log("You win!")
+   } else if (humanChoice === "scissors" && computerChoice === "paper"){
+    console.log("You win!")
+   } else {
+    console.log("You lost!")
+   }
+   console.log("You->", humanChoice, "|", "Computer->", computerChoice)
+
+
+}
+console.log(humanScore)
+const computer = getComputerChoice()
+const human = getHumanChoice()
+
+playRound(human, computer);
+
+function playGame(){
+
+}
